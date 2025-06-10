0d6dd9bc7bbeecd3be70b5ac3e3e4a8ec4b4f9eb
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2025-06-10 21:06:33 +0200

    feat: improve focus effect when hover

--- src/components/SkipCard.js
+++ src/components/SkipCard.js
@@ -6,7 +6,7 @@ import AllowedOnRoad from './AllowedOnRoad';
 
 export default function SkipCard({ title, price, hirePeriod, allowedOnRoad, children, isSelected }) {
   return (
-    <div className={`backdrop-blur-[10px] bg-white relative overflow-hidden rounded-2xl shadow-xl border hover:ring-2 hover:ring-blue-400 transition-all duration-300 p-6 space-y-4 hover:bg-opacity-100 ${allowedOnRoad ? 'border-blue-500' : 'border-red-400'} ${isSelected ? 'bg-opacity-100 font-bold':'bg-opacity-55'}`}>
+    <div className={`hover:scale-125 hover:z-50 ease-in-out backdrop-blur-[10px] bg-white relative overflow-hidden rounded-2xl shadow-xl border hover:ring-2 hover:ring-blue-400 transition-all duration-300 p-6 space-y-4 hover:bg-opacity-100 ${allowedOnRoad ? 'border-blue-500' : 'border-red-400'} ${isSelected ? 'bg-opacity-100 font-bold':'bg-opacity-55'}`}>
       <div className="flex justify-between items-center">
         <Title title={title} />
         <Price price={price} />
