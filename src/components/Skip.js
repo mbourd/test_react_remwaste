fcc247d54e65a2fae028db345b6cc1398f4b2c87
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2025-06-11 15:39:25 +0200

    feat: add cursor pointer on Skip component

--- src/components/Skip.js
+++ src/components/Skip.js
@@ -14,11 +14,13 @@ export default function Skip({ skip, setSelectedSkip, selectedSkip, isSelected }
     }
   };
   return (
-    <SkipCard title={skip.size} price={skip.price_before_vat} hirePeriod={skip.hire_period_days} allowedOnRoad={skip.allowed_on_road} isSelected={isSelected}>
-      <button className={`w-full text-white py-2 rounded-lg mt-4 ${isSelected ? 'bg-green-600 hover:bg-green-700' : 'bg-blue-600 hover:bg-blue-700'}`} onClick={handleSelect}>
-        {isSelected ? 'Deselect' : 'Select'} this Skip
-        <span className="ml-2">{isSelected ? '✓' : ''}</span>
-      </button>
-    </SkipCard>
+    <div className="hover:cursor-pointer" onClick={handleSelect}>
+      <SkipCard title={skip.size} price={skip.price_before_vat} hirePeriod={skip.hire_period_days} allowedOnRoad={skip.allowed_on_road} isSelected={isSelected}>
+        <button className={`w-full text-white py-2 rounded-lg mt-4 ${isSelected ? 'bg-green-600 hover:bg-green-700' : 'bg-blue-600 hover:bg-blue-700'}`} onClick={handleSelect}>
+          {isSelected ? 'Deselect' : 'Select'} this Skip
+          <span className="ml-2">{isSelected ? '✓' : ''}</span>
+        </button>
+      </SkipCard>
+    </div>
   );
 }
