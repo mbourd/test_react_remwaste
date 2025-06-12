da7174c51b03bb61cd41ad23322d88227e0185ab
Maxime Bourdin <maxime.bourdin.tic@gmail.com>
2025-06-12 13:40:13 +0200

    feat: add image

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ export default function App() {
             <p className="text-lg text-gray-600 mt-4">Select the best skip size based on your project needs</p>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {skips.map((skip) => (
               <Skip key={skip.id} skip={skip} setSelectedSkip={setSelectedSkip} selectedSkip={selectedSkip} isSelected={selectedSkip?.id === skip.id} />
             ))}
--- src/components/SkipCard.js
+++ src/components/SkipCard.js
@@ -3,18 +3,26 @@ import HirePeriod from './HirePeriod';
 import Title from './Title';
 import Price from './Price';
 import AllowedOnRoad from './AllowedOnRoad';
+import myImage from "../assets/img/18-600x600.jpg";
 
 export default function SkipCard({ title, price, hirePeriod, allowedOnRoad, children, isSelected }) {
   return (
-    <div className={`hover:scale-125 hover:z-50 ease-in-out backdrop-blur-[10px] bg-white relative overflow-hidden rounded-2xl shadow-xl border hover:ring-2 hover:ring-blue-400 transition-all duration-300 p-6 space-y-4 hover:bg-opacity-100 ${allowedOnRoad ? 'border-blue-500' : 'border-red-400'} ${isSelected ? 'bg-opacity-100 font-bold':'bg-opacity-55'}`}>
-      <div className="flex justify-between items-center">
-        <Title title={title} />
-        <Price price={price} />
+    <div className={`hover:scale-125 hover:z-50 ease-in-out backdrop-blur-[10px] bg-white relative overflow-hidden rounded-2xl shadow-xl border hover:ring-2 hover:ring-blue-400 transition-all duration-300 p-6 space-y-4 hover:bg-opacity-100 ${allowedOnRoad ? 'border-blue-500' : 'border-red-400'} ${isSelected ? 'bg-opacity-100 font-bold' : 'bg-opacity-55'}`}>
+      <div className="flex w-full h-full">
+        <div className="w-1/3 h-full mr-4">
+          <img src={myImage} alt="" className="w-full h-32 object-cover rounded-lg mb-4" />
+        </div>
+        <div className="w-2/3 h-full">
+          <div className="flex justify-between items-stretch">
+            <Title title={title} />
+            <Price price={price} />
+          </div>
+          <HirePeriod hirePeriod={hirePeriod} />
+          <AllowedOnRoad allowedOnRoad={allowedOnRoad} />
+          {children}
+          <div className="absolute bottom-0 left-0 h-1 w-full bg-gradient-to-r from-blue-400 to-blue-700"></div>
+        </div>
       </div>
-      <HirePeriod hirePeriod={hirePeriod} />
-      <AllowedOnRoad allowedOnRoad={allowedOnRoad} />
-      {children}
-      <div className="absolute bottom-0 left-0 h-1 w-full bg-gradient-to-r from-blue-400 to-blue-700"></div>
     </div>
   )
 }
